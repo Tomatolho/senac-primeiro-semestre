a535bcd09716aec2c411b477c595876bb3158534
Tomatolho <tomatevermelho10@gmail.com>
2026-08-19 11:54:12 -0300

    Add files via upload

---
+++ livros.java
@@ -0,0 +1,11 @@
+package Aulas;
+public class livros {
+    public static void main(String [] args) {
+        System.out.println("Livros favoritos:");
+        System.out.println("Assassinato No Expresso Oriente");
+        System.out.println("O Nevoeiro");
+        System.out.println("Maus");
+        System.out.println("Rei Lear");
+        System.out.println("Fausto");
+    } 
+}
