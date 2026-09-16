1e5f1ab292f6e3dae73bce91ec2881dfce5dd4c5
Tomatolho <tomatevermelho10@gmail.com>
2026-09-16 14:37:00 +0000

    16/09

---
+++ aula5/contador.java
@@ -0,0 +1,21 @@
+package aula5;
+import java.util.Scanner;
+public class contador {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int contador = 0;
+        int resto;
+
+        System.out.print("Digite um número");
+        int n1 = input.nextInt();
+
+        while (contador <= n1) {
+            resto = contador % 2;
+                if (resto == 0) {
+                    System.out.println(contador);
+                }
+            contador++;
+        }
+    }
+}
